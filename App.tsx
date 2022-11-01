29308d7d31f3b24f1051a09640d06f487f2a9aba
Renato Brandão <renato.silva@liveuniversity.com>
2022-11-01 01:59:42 -0300

    [FEAT] init

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { THEME } from "./src/style/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { SigIn } from "./src/screens/SignIn";
+import { Loading } from "./src/components/Loading";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SigIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
